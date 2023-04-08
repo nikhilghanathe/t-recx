cea8fe81cd6b867a0e600b1f982bdb61f53eac15
nikhilghanathe <nikhilghanathe@gmail.com>
2023-04-07 21:03:36 -0700

    train results ic sync, ic seems to be working

--- keyword_spotting/run_job_ev_assist.sh
+++ keyword_spotting/run_job_ev_assist.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#SBATCH --job-name tinyml-kws_model_ev_assist_point2
+#SBATCH --job-name tinyml-kws_model_ev_assist_point6
 #SBATCH --time 30:00:00
-#SBATCH --output=output_model_ev_assist_point2.txt
+#SBATCH --output=output_model_ev_assist_point6.txt
 nvidia-smi -q | grep -E '(Name|UUID)'
 eval "$(conda shell.bash hook)"
 conda activate trecx
 
-python train_dscnn.py --data_dir=data/ --model_architecture=ds_cnn_ev --isTrecx --isEV --model_save_name=model_with_ev_assist_point2 --W_aux=0.2
+python train_dscnn.py --data_dir=data/ --model_architecture=ds_cnn_ev --isTrecx --isEV --model_save_name=model_with_ev_assist_point6 --W_aux=0.6
 
--- keyword_spotting/run_job_noev_assist.sh
+++ keyword_spotting/run_job_noev_assist.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#SBATCH --job-name tinyml-kws_model_noev_assist_point2
+#SBATCH --job-name tinyml-kws_model_noev_assist_point6
 #SBATCH --time 30:00:00
-#SBATCH --output=output_model_noev_assist_point2.txt
+#SBATCH --output=output_model_noev_assist_point6.txt
 nvidia-smi -q | grep -E '(Name|UUID)'
 eval "$(conda shell.bash hook)"
 conda activate trecx
 
-python train_dscnn.py --data_dir=data/ --model_architecture=ds_cnn_noev --isTrecx  --model_save_name=model_without_ev_assist_point4 --W-aux=0.2
+python train_dscnn.py --data_dir=data/ --model_architecture=ds_cnn_noev --isTrecx  --model_save_name=model_without_ev_assist_point6 --W-aux=0.6
 
