148fc2d6769bddd465386d25198ead7d51d63d34
nikhilghanathe <nikhilghanathe@gmail.com>
2023-04-09 04:09:40 +0530

    train setup kws: corrected ee loss=None

--- keyword_spotting/keras_model.py
+++ keyword_spotting/keras_model.py
@@ -607,7 +607,7 @@ def get_model(args):
       #optimizer=keras.optimizers.RMSprop(learning_rate=args.learning_rate),  # Optimizer
       optimizer=keras.optimizers.Adam(learning_rate=args.learning_rate),  # Optimizer
       # Loss function to minimize
-      loss=keras.losses.SparseCategoricalCrossentropy(),
+      loss=[None, keras.losses.SparseCategoricalCrossentropy()],
       loss_weights = loss_weights,
       # List of metrics to monitor
       metrics=[keras.metrics.SparseCategoricalAccuracy()],
