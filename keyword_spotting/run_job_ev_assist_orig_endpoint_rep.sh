c317dc8f06559b32eab128cdfb46c52b3c0b9ea3
nikhilghanathe <nikhilghanathe@gmail.com>
2023-04-08 18:31:24 -0700

    train results vww after correction

--- keyword_spotting/run_job_ev_assist_orig_endpoint_rep.sh
+++ keyword_spotting/run_job_ev_assist_orig_endpoint_rep.sh
@@ -6,5 +6,5 @@ nvidia-smi -q | grep -E '(Name|UUID)'
 eval "$(conda shell.bash hook)"
 conda activate trecx
 
-python train_dscnn.py --data_dir=data/ --model_architecture=ds_cnn_ev --isAll_transf --isTrecx --isEV --model_save_name=model_with_ev_assist_ee_at_half_alltransf_point2 --W_aux=0.2 
+python train_dscnn.py --data_dir=data/ --model_architecture=ds_cnn_ev --isAll_transf --isTrecx --isEV --model_save_name=model_with_ev_assist_ee_at_half_alltransf_point2 --W_aux=0.2
 
